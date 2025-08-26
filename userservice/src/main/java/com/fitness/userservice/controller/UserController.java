4a30e55a4f6f1e3eadf2a0d03f644926b3889c31
saumyaa03 <63736923+saumyaa03@users.noreply.github.com>
2025-08-26 19:45:37 -0400

    Implement User Service with user registration and profile retrieval endpoints, using DTOs (RegisterRequest, UserResponse) and PostgreSQL integration

--- userservice/src/main/java/com/fitness/userservice/controller/UserController.java
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -5,17 +5,20 @@ import com.fitness.userservice.dto.UserResponse;
 import com.fitness.userservice.services.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 @AllArgsConstructor
 public class UserController {
     private UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponse> getUserProfile(@PathVariable String userId) {
+        return ResponseEntity.ok(userService.getUserProfile(userId));
+    }
 
     @PostMapping("/register")
     public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request) {
