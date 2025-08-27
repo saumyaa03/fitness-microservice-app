308bf48528c63d28dfc763df8ddfc41a99acb7b3
saumyaa03 <63736923+saumyaa03@users.noreply.github.com>
2025-08-27 15:29:43 -0400

    feat(activity-service): add initial Activity Service backend with MongoDB integration

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,14 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    SWIMMING,
+    CYCLING,
+    WEIGHT_TRAINING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    OTHER
+}
